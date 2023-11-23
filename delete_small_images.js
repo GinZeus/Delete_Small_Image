bb7bebd9e2b50fe75025123fac0f23f7e481b7b6
GinZeus <datnguyen2572003@gmail.com>
2023-11-23 13:44:17 +0700

    delete img root, child forder

--- delete_small_images.js
+++ delete_small_images.js
@@ -2,43 +2,49 @@ const fs = require('fs');
 const path = require('path');
 const Jimp = require('jimp');
 
-const directory = 'D:/code/Kotora/videoAI/Filtered_Dataset_IPH_3.11'; // Thay đổi đường dẫn thư mục của bạn
+const rootDirectory = 'D:/code/Kotora/videoAI/Data_16-11_Indoor/log_cam4'; // Thay đổi đường dẫn thư mục gốc của bạn
 const widthThreshold = 128;
 const heightThreshold = 256;
 
-fs.readdir(directory, (err, files) => {
-  if (err) {
-    console.error('Error reading directory:', err);
-    return;
-  }
+function deleteSmallImages(directory) {
+  fs.readdir(directory, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return;
+    }
 
-  files.forEach((file) => {
-    const filePath = path.join(directory, file);
-    fs.stat(filePath, (err, stats) => {
-      if (err) {
-        console.error(`Error retrieving file stats for ${file}:`, err);
-        return;
-      }
+    files.forEach((file) => {
+      const filePath = path.join(directory, file);
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          console.error(`Error retrieving file stats for ${file}:`, err);
+          return;
+        }
 
-      if (stats.isFile()) {
-        Jimp.read(filePath, (err, image) => {
-          if (err) {
-            console.error(`Error processing image ${file}:`, err);
-            return;
-          }
+        if (stats.isFile()) {
+          Jimp.read(filePath, (err, image) => {
+            if (err) {
+              console.error(`Error processing image ${file}:`, err);
+              return;
+            }
 
-          const { width, height } = image.bitmap;
-          if (width < widthThreshold || height < heightThreshold) {
-            fs.unlink(filePath, (err) => {
-              if (err) {
-                console.error(`Error deleting image ${file}:`, err);
-              } else {
-                console.log(`Deleted ${file}`);
-              }
-            });
-          }
-        });
-      }
+            const { width, height } = image.bitmap;
+            if (width < widthThreshold || height < heightThreshold) {
+              fs.unlink(filePath, (err) => {
+                if (err) {
+                  console.error(`Error deleting image ${file}:`, err);
+                } else {
+                  console.log(`Deleted ${file}`);
+                }
+              });
+            }
+          });
+        } else if (stats.isDirectory()) {
+          deleteSmallImages(filePath); // Đệ quy vào thư mục con
+        }
+      });
     });
   });
-});
+}
+
+deleteSmallImages(rootDirectory);
